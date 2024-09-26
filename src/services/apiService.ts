6a8eb0225f4aec4d31463c9050bc299cb0c2c1f9
trsaints <thiago.dev2k3@gmail.com>
2024-09-26 17:29:18 -0300

    feat: create service for API authentication and data access

---
+++ src/services/apiService.ts
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+
+async function getApiToken() {
+	const clientId = import.meta.env.VITE_API_CLIENT_ID as string,
+		  clientSecret   = import.meta.env.VITE_API_CLIENT_SECRET as string,
+		  apiUrl = `https://id.twitch.tv/oauth2/token`
+	
+	const result = await axios.post(apiUrl, null,  {
+		params: {
+			client_id: clientId,
+			client_secret: clientSecret,
+			grant_type: 'client_credentials'
+		}
+	})
+	
+	return result.data?.access_token
+}
+
+export const apiService = {
+	getApiToken
+}
