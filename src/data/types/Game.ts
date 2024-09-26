17c43a5c9c303911eb0d332e916a7d7392437a0c
trsaints <thiago.dev2k3@gmail.com>
2024-09-26 17:02:09 -0300

    feat: define the actual properties of API endpoints to be used

--- src/data/types/Game.ts
+++ src/data/types/Game.ts
@@ -1,1 +1,41 @@
-export type Game = {}
+export type Game = {
+	name: string
+	genres: number[]
+	category: GameCategory
+	firstReleaseDate: number
+	involvedCompanies: number[]
+	platforms: number[]
+	status: GameStatus
+	totalRating: number
+	similarGames: number[]
+	summary: string
+}
+
+export enum GameCategory {
+	mainGame,
+	dlcAddon,
+	expansion,
+	bundle,
+	standaloneExpansion,
+	mod,
+	episode,
+	season,
+	remake,
+	remaster,
+	expandedGame,
+	port,
+	fork,
+	update,
+	pack,
+}
+
+export enum GameStatus {
+	released,
+	alpha,
+	beta,
+	early_access,
+	offline,
+	cancelled,
+	rumored,
+	delisted,
+}
