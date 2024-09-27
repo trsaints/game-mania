e9a4f3f6418188812e347fb6b039e1bf5f9f460d
trsaints <thiago.dev2k3@gmail.com>
2024-09-27 08:23:44 -0300

    feat: add base structure for the GamePage route

--- src/view/routes/Game/GamePage.tsx
+++ src/view/routes/Game/GamePage.tsx
@@ -2,9 +2,56 @@ import style         from './GamePage.module.scss'
 import { IGamePage } from './IGamePage.ts'
 
 
-function GamePage(props: IGamePage) {
+function GamePage({game}: IGamePage) {
+	const gameCompanies = game.involvedCompanies.map(
+		(c, i) => (<li key={`${c}-${i}`}>{c}</li>))
+	
+	const gameGenres = game.genres.map(
+		(g,i) => (<li key={`${g}-${i}`}>{g}</li>))
+
+	const gamePlatforms = game.platforms.map(
+		(p, i) =>  (<li key={`${p}-${i}`}>{p}</li>))
+	
+	const similarGames = game.similarGames.map(
+		(g, i) => (<li key={`${g}-${i}`}>{g}</li>))
+
 	return (
 		<article className={style.GamePage}>
+			<header>
+				<h2>{game.name}</h2>
+				<p>Released at: {game.firstReleaseDate}</p>
+
+				<p>
+					Genres:
+					<ul>{gameGenres}</ul>
+				</p>
+				
+				<p>
+					Made by: 
+					<ul>{gameCompanies}</ul>
+				</p>
+				
+				<p>Rating: {game.totalRating}</p>
+			</header>
+
+			<p>{game.summary}</p>
+
+			<aside>
+				<h2>Details</h2>
+				<p>Status {game.status}</p>
+				<p>Category: {game.category}</p>
+				
+				<p>
+					Platforms: 
+					<ul>{gamePlatforms}</ul>
+				</p>
+			</aside>
+			
+			<article>
+				<h2>You might also like</h2>
+				
+				<ul>{similarGames}</ul>
+			</article>
 		</article>
 	)
 }
