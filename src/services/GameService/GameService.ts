c16c4baee7170587eb33a89e56abae93e9fd2339
trsaints <thiago.dev2k3@gmail.com>
2024-09-28 14:20:37 -0300

    feat: create a new service for obtaining game data

---
+++ src/services/GameService/GameService.ts
@@ -0,0 +1,28 @@
+import { Game } from '@data/types'
+import { ApiService } from '@services/ApiService'
+
+export const GameService = {
+    getGames,
+    getGameById
+}
+
+const baseUrl = ApiService.createRouteUrl("games")
+
+async function getGames(): Promise<Game[]> {
+    const mapToGameArray = (results: any[]) => 
+        results.map((r: Game) => r as Game)
+    
+    const response = await ApiService.gameApi.get(baseUrl)
+    const games = mapToGameArray(response.data.results)
+    
+    if (games === undefined) return []
+    
+    return games
+}
+
+async function getGameById(id: number): Promise<Game> {
+    const gameUrl = ApiService.createRouteUrl(`${baseUrl}/${id}`)
+    const response = await ApiService.gameApi.get(gameUrl)
+    
+    return response.data as Game
+}
--- src/views/routes/Game/IGamePage.ts
+++ src/views/routes/Game/IGamePage.ts
@@ -1,7 +1,5 @@
 import { ComponentProps } from 'react'
-import { Game }           from '../../../data/types/Game.ts'
 
 
 export interface IGamePage extends ComponentProps<'article'> {
-	game: Game
 }
