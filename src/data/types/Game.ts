4e4c61c6c20656846ce58f0c2e352d507b0d1a2b
trsaints <thiago.dev2k3@gmail.com>
2024-09-28 14:19:32 -0300

    refactor: rebuild main data model type for matching new API modeling

--- src/data/types/Game.ts
+++ src/data/types/Game.ts
@@ -1,41 +1,47 @@
+import { Platforms } from '@data/types'
+
+
 export type Game = {
+	/* "id": 0,
+	 "slug": "string",
+	 "name": "string",
+	 "released": "2024-09-28",
+	 "tba": true,
+	 "background_image": "http://example.com",
+	 "rating": 0,
+	 "rating_top": 0,
+	 "ratings": { },
+	 "ratings_count": 0,
+	 "reviews_text_count": "string",
+	 "added": 0,
+	 "added_by_status": { },
+	 "metacritic": 0,
+	 "playtime": 0,
+	 "suggestions_count": 0,
+	 "updated": "2024-09-28T16:16:49Z",*/
+	
+	id: number
+	slug: string
 	name: string
-	genres: number[]
-	category: GameCategory
-	firstReleaseDate: number
-	involvedCompanies: number[]
-	platforms: number[]
-	status: GameStatus
-	totalRating: number
-	similarGames: number[]
-	summary: string
+	released: string
+	tba: boolean
+	backgroundImage: string
+	rating: number
+	ratingTop: number
+	ratingsCount: number
+	reviewsTextCount: string
+	added: number
+	addedByStatus: any
+	metacritic: number
+	playtime: number
+	suggestionsCount: 0
+	updated: string
+	esrbRating: {
+		id: number
+		slug: "everyone"
+		name: "Everyone"
+	}
+	platforms: Platforms
 }
 
-export enum GameCategory {
-	mainGame,
-	dlcAddon,
-	expansion,
-	bundle,
-	standaloneExpansion,
-	mod,
-	episode,
-	season,
-	remake,
-	remaster,
-	expandedGame,
-	port,
-	fork,
-	update,
-	pack,
-}
 
-export enum GameStatus {
-	released,
-	alpha,
-	beta,
-	early_access,
-	offline,
-	cancelled,
-	rumored,
-	delisted,
-}
