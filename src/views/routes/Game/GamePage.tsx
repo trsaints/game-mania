26283418731d90e685c44eb6dcb2d9d21c869cf6
trsaints <thiago.dev2k3@gmail.com>
2024-09-30 13:11:46 -0300

    remove: temporarly remove unused structure

--- src/views/routes/Game/GamePage.tsx
+++ src/views/routes/Game/GamePage.tsx
@@ -1,52 +1,10 @@
-import { GameContext } from '@data/context'
-import { useContext }  from 'react'
-import style           from './GamePage.module.scss'
+import style from './GamePage.module.scss'
 
 
 function GamePage() {
-	const {selectedGame} = useContext(GameContext)
-
-	const gameGenres = selectedGame?.genres.map(
-		(g) => (<li key={`${g.slug}-${g.id}`}>{g.name}</li>))
-
-	const gamePlatforms = selectedGame?.platforms?.platforms.map(
-		(p) => (<li key={`${p.slug}-${p.id}`}>{p.name}</li>))
-
-	const gameTags = selectedGame?.tags.map(
-		(t) => (<li key={`${t.slug}-${t.id}`}>{t.name}</li>))
-
 	return (
 		<article className={style.GamePage}>
-			<header>
-				<h2>{selectedGame?.name}</h2>
-				<p>Released at: {selectedGame?.released}</p>
 
-				<p>
-					Genres:
-					<ul>{gameGenres}</ul>
-				</p>
-
-				<p>Rating: {selectedGame?.rating}/{selectedGame?.ratingTop}</p>
-			</header>
-
-			<aside>
-				<h2>Details</h2>
-				<p>
-					Categories:
-					<ul>{gameTags}</ul>
-				</p>
-
-				<p>
-					Platforms:
-					<ul>{gamePlatforms}</ul>
-				</p>
-			</aside>
-
-			<article>
-				<h2>You might also like</h2>
-
-				<ul>{gameTags}</ul>
-			</article>
 		</article>
 	)
 }
