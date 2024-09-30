bd0eb87776b96b3e8907ffba3b4630558e61939a
trsaints <thiago.dev2k3@gmail.com>
2024-09-29 21:40:45 -0300

    feat: add base stylesheet for the Home route

--- src/views/routes/Home/Home.tsx
+++ src/views/routes/Home/Home.tsx
@@ -1,20 +1,41 @@
 import { GameContext } from '@data/context'
 import { GameCard }    from '@views/components'
 import { useContext }  from 'react'
+import { Link }        from 'react-router-dom'
 import style           from './Home.module.scss'
 
 
 function Home() {
 	const gameContext = useContext(GameContext)
-	
+
 	const gameList = gameContext.games
-								.map(g => <li key={g.id}><GameCard game={g}/></li>)
-	
+								.map(g =>
+										 (<li key={g.id}>
+											 <GameCard game={g}/>
+										 </li>))
+
 	return (
 		<article className={style.Home}>
-			<h2>Welcome</h2>
+			<h2>Navigate by genre</h2>
+
+			<menu className={style.Menu}>
+				<li>
+					<button className={style.Option}>action</button>
+				</li>
+				<li>
+					<button className={style.Option}>adventure</button>
+				</li>
+				<li>
+					<button className={style.Option}>platform</button>
+				</li>
+				<li>
+					<button className={style.Option}>rpg</button>
+				</li>
+			</menu>
 
 			<ul>{gameList}</ul>
+
+			<Link to="/search">see all</Link>
 		</article>
 	)
 }
