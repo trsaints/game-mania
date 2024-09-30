2abeb6c1980f4b203e01dea8767c0ba7b01ce55a
trsaints <thiago.dev2k3@gmail.com>
2024-09-30 13:17:40 -0300

    refactor: unify types given minor difference between both

--- src/data/types/Game.ts
+++ src/data/types/Game.ts
@@ -1,6 +1,33 @@
-import { GameCommons, ShortScreenshot } from '@data/types'
+import { Developer, GameCommons, Publisher, ShortScreenshot } from '@data/types'
 
 
 export type Game = GameCommons & {
-	shortScreenshots: ShortScreenshot[]
+	nameOriginal: string
+	description: string
+	metacriticPlatforms: unknown[]
+	backgroundImageAdditional: string
+	website: string
+	screenshotsCount: number
+	moviesCount: number
+	creatorsCount: number
+	achievementsCount: number
+	parentAchievementsCount: number
+	redditUrl: string
+	redditName: string
+	redditDescription: string
+	redditLogo: string
+	redditCount: number
+	twitchCount: number
+	youtubeCount: number
+	alternativeNames: string[]
+	metacriticUrl: string
+	parentsCount: number
+	additionsCount: number
+	gameSeriesCount: number
+	developers: Developer[]
+	publishers: Publisher[]
+	descriptionRaw: string
+	shortScreenshots?: ShortScreenshot[]
 }
+
+
--- src/services/GameService/GameService.ts
+++ src/services/GameService/GameService.ts
@@ -1,43 +1,43 @@
-import { Game } from '@data/types'
-import { GameDetails } from '@data/types/GameDetails.ts'
-import { ApiService } from '@services/ApiService'
+import { Game }        from '@data/types'
+import { ApiService }  from '@services/ApiService'
 import { ParserUtils } from '@src/utils'
 
+
 export const GameService = {
-    getGames,
-    getGameById
+	getGames,
+	getGameById
 }
 
 async function getGames(): Promise<Game[]> {
-    const baseUrl = ApiService.createRouteUrl("games")
-    const response = await ApiService.gameApi.get(baseUrl)
-    
-    return response.data?.results.map(mapToGameType<Game>) ?? []
+	const baseUrl  = ApiService.createRouteUrl('games')
+	const response = await ApiService.gameApi.get(baseUrl)
+
+	return response.data?.results.map(mapToGameType) ?? []
 }
 
-async function getGameById(id: number): Promise<GameDetails> {
-    const gameUrl = ApiService.createRouteUrl(`games/${id}`)
-    const response = await ApiService.gameApi.get(gameUrl)
-    
-    return mapToGameType<GameDetails>(response.data)
+async function getGameById(id: number): Promise<Game> {
+	const gameUrl  = ApiService.createRouteUrl(`games/${id}`)
+	const response = await ApiService.gameApi.get(gameUrl)
+
+	return mapToGameType(response.data)
 }
 
-function mapToGameType<T>(data: any): T {
-    let mappedGame: any = {}
-    const dataEntries = Object.entries(data)
-    
-    dataEntries.forEach(([key, value]) => {
-        if (!key.includes('_')) {
-            mappedGame[key] = value
-            
-            return
-        } 
-        
-        const camelCasedKey = ParserUtils.convertSnakeToCamelCase(key)
-        
-        mappedGame[camelCasedKey] = value
-    })
-    
-    return mappedGame as T
+function mapToGameType(data: any): Game {
+	let mappedGame: any = {}
+	const dataEntries   = Object.entries(data)
+
+	dataEntries.forEach(([key, value]) => {
+		if (!key.includes('_')) {
+			mappedGame[key] = value
+
+			return
+		}
+
+		const camelCasedKey = ParserUtils.convertSnakeToCamelCase(key)
+
+		mappedGame[camelCasedKey] = value
+	})
+
+	return mappedGame as Game
 }
 
