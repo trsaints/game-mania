62bd1352df3ac2cf3630dcab6ccc997ffe26a548
trsaints <thiago.dev2k3@gmail.com>
2024-10-01 09:22:54 -0300

    feat: display GamePanel components upon random recommendations

--- src/views/routes/Home/Home.tsx
+++ src/views/routes/Home/Home.tsx
@@ -1,45 +1,65 @@
-import { GameContext } from '@data/context'
-import { GameCard }    from '@views/components'
-import { useContext }  from 'react'
-import { Link }        from 'react-router-dom'
-import style           from './Home.module.scss'
+import { GameContext }                     from '@data/context'
+import { Recommended }                     from '@data/types/Recommended.ts'
+import {
+	GameService
+}                                          from '@services/GameService/GameService.ts'
+import { GameCard }                        from '@views/components'
+import { GamePanel }                       from '@views/components/GamePanel'
+import { useContext, useEffect, useState } from 'react'
+import { Link }                            from 'react-router-dom'
+import style                               from './Home.module.scss'
 
 
 function Home() {
-	const gameContext = useContext(GameContext)
+	const [recommended, setRecommended] = useState<Recommended>()
 
+	useEffect(() => {
+		GameService.getRecommendations().then(r => setRecommended(r))
+	}, [])
+
+	const gameContext = useContext(GameContext)
 	const gameList = gameContext.games
 								.map(g =>
 										 (<li key={g.id}>
 											 <GameCard game={g}/>
 										 </li>))
 
+
 	return (
 		<article className={style.Home}>
-			<h2>Navigate by genre</h2>
+			<h2>Welcome</h2>
 
-			<menu className={style.Menu}>
-				<li>
-					<button className={style.Option}>action</button>
-				</li>
-				<li>
-					<button className={style.Option}>adventure</button>
-				</li>
-				<li>
-					<button className={style.Option}>platform</button>
-				</li>
-				<li>
-					<button className={style.Option}>rpg</button>
-				</li>
-			</menu>
+			{recommended?.recent && <GamePanel game={recommended.recent}/>}
 
-			<ul className={style.GameList}>
-				{gameList}
-				<li>
-					<Link to="/search">see all</Link>
-				</li>
-			</ul>
+			<article>
+				<h3>Navigate by genre</h3>
 
+				<menu className={style.Menu}>
+					<li>
+						<button className={style.Option}>action</button>
+					</li>
+					<li>
+						<button className={style.Option}>adventure</button>
+					</li>
+					<li>
+						<button className={style.Option}>platform</button>
+					</li>
+					<li>
+						<button className={style.Option}>rpg</button>
+					</li>
+				</menu>
+
+				<ul className={style.GameList}>
+					{gameList}
+					<li>
+						<Link to="/search">see all</Link>
+					</li>
+				</ul>
+			</article>
+
+			<h2>Daily Suggestion</h2>
+
+			{recommended?.daily && <GamePanel game={recommended.daily}/>}
 		</article>
 	)
 }
