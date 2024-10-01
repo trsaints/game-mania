ded588c0caeaaad7a546788375de4e5725cc014b
trsaints <thiago.dev2k3@gmail.com>
2024-10-01 14:09:59 -0300

    feat: add base styling to the GamePanel component

--- src/views/components/GamePanel/GamePanel.tsx
+++ src/views/components/GamePanel/GamePanel.tsx
@@ -8,7 +8,7 @@ function GamePanel({game, screenshots}: IGamePanel) {
 	return (
 		<article className={style.GamePanel}>
 			<Header game={game}/>
-			<Banner screenshots={screenshots}/>
+			<Banner name={game.name} screenshots={screenshots}/>
 		</article>
 	)
 }
@@ -19,53 +19,70 @@ interface IHeader extends PropsWithChildren {
 
 function Header({game}: IHeader) {
 	const gameGenres = game.genres.slice(0, 3)
-						   .map(g => <li key={`t-${g.id}`}>{g.name}</li>)
+						   .map(g => <li className={style.GenreTag} key={`t-${g.id}`}>{g.name}</li>)
 
 	return (
-		<header>
+		<header className={style.Header}>
 			<h3>{game.name}</h3>
 
-			<p>Released at:
+			<p className={style.Released}>Released at: {' '}
 				<time dateTime={game.released}>
 					{new Date(game.released).getFullYear()}
 				</time>
 			</p>
 
-			<p>Publisher: {game?.publishers[0]?.name}</p>
+			<p className={style.Publisher}>Publisher: {game?.publishers[0]?.name}</p>
 
-			<p>{game.descriptionRaw}</p>
+			<p className={style.Description}>{game.descriptionRaw}</p>
 
-			<ul>{gameGenres}</ul>
+			<ul className={style.Tags}>{gameGenres}</ul>
 		</header>
 	)
 }
 
 interface IBanner extends PropsWithChildren {
+	name: string
 	screenshots: Screenshots
 }
 
-function Banner({screenshots}: IBanner) {
-	return (<article>
-		<figure>
-			<img src={screenshots?.results[0].image ?? ''} alt=""/>
-			<figcaption></figcaption>
-		</figure>
+function Banner({name, screenshots}: IBanner) {
+	return (
+		<article>
+			<figure>
+				<img className={style.Banner}
+					 src={screenshots?.results[0].image ?? ''}
+					 alt=""
+				/>
+				<figcaption>{`Game banner for the "${name}" game`}</figcaption>
+			</figure>
 
-		<menu>
-			<li>
-				<img src={screenshots?.results[1].image ?? ''} alt=""/>
-				<button>Click to see</button>
-			</li>
-			<li>
-				<img src={screenshots?.results[2].image ?? ''} alt=""/>
-				<button>Click to see</button>
-			</li>
-			<li>
-				<img src={screenshots?.results[3].image ?? ''} alt=""/>
-				<button>Click to see</button>
-			</li>
-		</menu>
-	</article>)
+			<menu className={style.Screenshots}>
+				<li>
+					<img className={style.Banner}
+						 src={screenshots?.results[1].image ?? ''}
+						 alt=""
+					/>
+					<button>Click to see</button>
+				</li>
+				
+				<li>
+					<img className={style.Banner}
+						 src={screenshots?.results[2].image ?? ''}
+						 alt=""
+					/>
+					<button>Click to see</button>
+				</li>
+				
+				<li>
+					<img className={style.Banner}
+						 src={screenshots?.results[3].image ?? ''}
+						 alt=""
+					/>
+					<button>Click to see</button>
+				</li>
+			</menu>
+		</article>
+	)
 }
 
 export { GamePanel }
