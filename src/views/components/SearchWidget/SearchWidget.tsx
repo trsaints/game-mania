a45524add7529093c77ca2d8d85b366a10155961
trsaints <thiago.dev2k3@gmail.com>
2024-10-03 10:58:53 -0300

    refactor: breakdown main component for responding upon dynamic input

--- src/views/components/SearchWidget/SearchWidget.tsx
+++ src/views/components/SearchWidget/SearchWidget.tsx
@@ -1,64 +1,74 @@
-import { Form } from 'react-router-dom'
-import style    from './SearchWidget.module.scss'
+import { Genre, Publisher, Tag } from '@data/types'
+import { useState }              from 'react'
+import { Form, Link }            from 'react-router-dom'
+import style                     from './SearchWidget.module.scss'
 
 
 function SearchWidget() {
+	const [genres, setGenres]         = useState<Genre[]>([])
+	const [tags, setTags]             = useState<Tag[]>([])
+	const [publishers, setPublishers] = useState<Publisher[]>([])
+
+	const props: ISearchNavbar = {
+		genres,
+		tags,
+		publishers
+	}
+
 	return (
 		<aside className={style.SearchWidget}>
 			<SearchForm/>
-			<SearchNavbar/>
+			<SearchNavbar {...props} />
 		</aside>
 	)
 }
 
 function SearchForm() {
 	return (
-		<Form method="post" className={style.SearchForm}>
+		<Form method="get" className={style.SearchForm} action="/search">
 			<label className={style.Label} htmlFor="search">Keyword</label>
 			<input className={style.Search} type="search" id="search"/>
-			<button className={`primary ${style.Submit}`} type="submit">Search</button>
+			
+			<button className={`primary ${style.Submit}`}
+					type="submit"
+			>
+				Search
+			</button>
 		</Form>
 	)
 }
 
-function SearchNavbar() {
+interface ISearchNavbar {
+	genres: Genre[]
+	tags: Tag[]
+	publishers: Publisher[]
+}
+
+function SearchNavbar(props: ISearchNavbar) {
+	const getList = (data: Genre[] | Tag[] | Publisher[]) => data.map(d => {
+		const key = `search-${typeof (d)}-${d.id}`
+
+		return (<li key={key}><Link to="/search">{d.name}</Link></li>)
+	})
+
+	const getSection = (key: keyof ISearchNavbar) => {
+		return (
+			<li>
+				<details>
+					<summary>{key}</summary>
+
+					<ul className={style.SearchOptions}>
+						{getList(props[key])}
+					</ul>
+				</details>
+			</li>)
+	}
+
 	return (
 		<menu className={style.SearchMenu}>
-			<li>
-				<details>
-					<summary>genres</summary>
-
-					<ul className={style.SearchOptions}>
-						<li>action</li>
-						<li>adventure</li>
-						<li>strategy</li>
-					</ul>
-				</details>
-			</li>
-
-			<li>
-				<details>
-					<summary>tags</summary>
-
-					<ul className={style.SearchOptions}>
-						<li>singleplayer</li>
-						<li>mmo</li>
-						<li>cooperative</li>
-					</ul>
-				</details>
-			</li>
-
-			<li>
-				<details>
-					<summary>publishers</summary>
-
-					<ul className={style.SearchOptions}>
-						<li>rockstar games</li>
-						<li>cd projekt red</li>
-						<li>mojang</li>
-					</ul>
-				</details>
-			</li>
+			{getSection('genres')}
+			{getSection('tags')}
+			{getSection('publishers')}
 		</menu>
 	)
 }
