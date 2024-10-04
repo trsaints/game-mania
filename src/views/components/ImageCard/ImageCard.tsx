a79e6ebc83d588ca307fe9605edce41328b9e54e
trsaints <thiago.dev2k3@gmail.com>
2024-10-04 14:12:42 -0300

    update: reformat source-code

--- src/views/components/ImageCard/ImageCard.tsx
+++ src/views/components/ImageCard/ImageCard.tsx
@@ -4,14 +4,14 @@ import style          from './ImageCard.module.scss'
 
 function ImageCard({ img, alt }: IImageCard) {
 	return (
-        <p className={style.ImageCard}>
-            <button type='button'>click to see</button>
+		<p className={style.ImageCard}>
+			<button type="button">click to see</button>
 
-            <img src={img?.image ?? ''}
-                 alt={alt}
-            />
-        </p>
-    )
+			<img src={img?.image ?? ''}
+				 alt={alt}
+			/>
+		</p>
+	)
 }
 
 export { ImageCard }
