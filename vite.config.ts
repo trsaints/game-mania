cf837e10a0bd76a6603b9d95c4c89b88461d1e18
trsaints <thiago.dev2k3@gmail.com>
2024-10-04 09:52:01 -0300

    raw: map src/utils to module annotations

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths'
+import react            from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import tsconfigPaths    from 'vite-tsconfig-paths'
+
 
 export default defineConfig({
-                                plugins: [react(), tsconfigPaths()],
-                                resolve: {
-                                    alias: {
-                                        '@src': '/src',
-                                        '@data': '/src/data',
-                                        '@views': '/src/views',
-                                        '@services': '/src/services',
-                                    },
-                                },
-                            });
+								plugins: [react(), tsconfigPaths()],
+								resolve: {
+									alias: {
+										'@src'     : '/src',
+										'@data'    : '/src/data',
+										'@views'   : '/src/views',
+										'@services': '/src/services',
+										'@utils'   : '/src/utils'
+									}
+								}
+							})
