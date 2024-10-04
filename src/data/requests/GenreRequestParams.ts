b633242952776190ff5dfde791521a9ac3d4f428
trsaints <thiago.dev2k3@gmail.com>
2024-10-04 09:26:10 -0300

    feat: add request parameters for filtering Genre data requests

---
+++ src/data/requests/GenreRequestParams.ts
@@ -0,0 +1,5 @@
+export type GenreRequestParams = {
+	ordering?: string
+	page?: number
+	pageSize?: number
+}
--- src/services/GenreService/GenreService.ts
+++ src/services/GenreService/GenreService.ts
@@ -1,6 +1,7 @@
-import { Genre }       from '@data/types'
-import { ApiService }  from '@src/services'
-import { ParserUtils } from '@src/utils'
+import { GenreRequestParams } from '@data/requests'
+import { Genre }              from '@data/types'
+import { ApiService }         from '@src/services'
+import { ParserUtils }        from '@src/utils'
 
 
 export const GenreService = {
@@ -8,16 +9,18 @@ export const GenreService = {
 	getGenreById
 }
 
-async function getGenres(): Promise<Genre[]> {
+async function getGenres(params: GenreRequestParams): Promise<Genre[]> {
 	const baseUrl  = ApiService.createRouteUrl('genres')
-	const response = await ApiService.gameApi.get(baseUrl)
+	const response = await ApiService.gameApi.get(baseUrl, {
+		params: params ? ParserUtils.mapToSnakeCase(params) : {}
+	})
 
 	return response.data?.results.map((r: any) => ParserUtils.mapToCamelCase(r)) as Genre[]
 		   ?? []
 }
 
 async function getGenreById(id: number): Promise<Genre> {
-	const baseUrl = ApiService.createRouteUrl(`genres/${id}`)
+	const baseUrl  = ApiService.createRouteUrl(`genres/${id}`)
 	const response = await ApiService.gameApi.get(baseUrl)
 
 	return ParserUtils.mapToCamelCase(response?.data) as Genre
