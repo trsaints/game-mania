d3dbedffdc9acb8268cb164b38c2c994372b21fc
trsaints <thiago.dev2k3@gmail.com>
2024-10-04 13:51:29 -0300

    feat: create the Gallery component for displaying game screenshots

---
+++ src/views/components/Gallery/Gallery.tsx
@@ -0,0 +1,26 @@
+import { ImageCard } from '@views/components'
+import { Link }      from 'react-router-dom'
+import style         from './Gallery.module.scss'
+import { IGallery }  from './IGallery'
+
+
+export { Gallery }
+
+function Gallery({ screenshots }: IGallery) {
+	const cards = screenshots.results.map(i => (
+		<li key={`${i.id}-${typeof (i)}`}>
+			<ImageCard img={i} alt={''}/>
+		</li>)
+	)
+
+	return (
+		<aside className={style.Gallery}>
+			<h3>gallery</h3>
+
+			<ul>{cards}</ul>
+
+			<Link to="/search">see all</Link>
+		</aside>
+	)
+}
+
