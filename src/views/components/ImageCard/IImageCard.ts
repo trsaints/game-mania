49650ecd0c1ec3947bf39f2ec199698df4f713a2
trsaints <thiago.dev2k3@gmail.com>
2024-10-04 13:50:41 -0300

    refactor: extract ImageCard subcomponent into new independent component

---
+++ src/views/components/ImageCard/IImageCard.ts
@@ -0,0 +1,8 @@
+import { Image }          from '@data/types/Image.ts'
+import { ComponentProps } from 'react'
+
+
+export interface IImageCard extends ComponentProps<'p'> {
+	img: Image
+	alt: string
+}
