2c0b2b560406caef750be5f2a6a6835c3c37d500
trsaints <thiago.dev2k3@gmail.com>
2024-10-06 17:54:42 -0300

    fix: rollback data service to object literal structure, due to runtime not hoisting class methods properly

--- src/services/GameService.ts
+++ src/services/GameService.ts
@@ -16,94 +16,102 @@ import { IGameService }      from '@services/interfaces'
 import { ParserUtils }       from '@src/utils'
 
 
-export class GameService implements IGameService {
-	public async getAll(params?: GameRequestParams): Promise<Game[]> {
-		const baseUrl  = ApiService.createRouteUrl('games')
-		const response = await ApiService.gameApi.get(baseUrl, {
-			params: params ? ParserUtils.mapToSnakeCase(params) : {}
-		})
+export const GameService: IGameService = {
+	getAll,
+	getById,
+	getRecommendations,
+	getScreenshots,
+	mapToGame,
+	mapToGamesPlatforms,
+	mapToStores
+}
 
-		return response.data?.results.map(this.mapToGame) ?? []
+async function getAll(params?: GameRequestParams): Promise<Game[]> {
+	const baseUrl  = ApiService.createRouteUrl('games')
+	const response = await ApiService.gameApi.get(baseUrl, {
+		params: params ? ParserUtils.mapToSnakeCase(params) : {}
+	})
+
+	return response.data?.results.map(mapToGame) ?? []
+}
+
+async function getById(id: number): Promise<Game> {
+	const gameUrl  = ApiService.createRouteUrl(`games/${id}`)
+	const response = await ApiService.gameApi.get(gameUrl)
+
+	return mapToGame(response.data)
+}
+
+async function getRecommendations(): Promise<Recommended> {
+	const recentId = Math.floor(Math.random() * 1000)
+	const dailyId  = Math.floor(Math.random() * 1000)
+
+	return {
+		daily            : await getById(dailyId),
+		recent           : await getById(recentId),
+		recentScreenshots: await getScreenshots(recentId),
+		dailyScreenshots : await getScreenshots(dailyId)
+	}
+}
+
+function mapToGame(data: any): Game {
+	const mappedData = ParserUtils.mapToCamelCase(data) as Game
+
+	if (mappedData.platforms !== undefined) {
+		mappedData.platforms
+			= mapToGamesPlatforms(mappedData.platforms)
 	}
 
-	public async getById(id: number): Promise<Game> {
-		const gameUrl  = ApiService.createRouteUrl(`games/${id}`)
-		const response = await ApiService.gameApi.get(gameUrl)
-
-		return this.mapToGame(response.data)
+	if (mappedData.developers) {
+		mappedData.developers = mappedData.developers.map(d => ParserUtils
+		.mapToCamelCase(d) as Developer)
 	}
 
-	public async getRecommendations(): Promise<Recommended> {
-		const recentId = Math.floor(Math.random() * 1000)
-		const dailyId  = Math.floor(Math.random() * 1000)
-
-		return {
-			daily            : await this.getById(dailyId),
-			recent           : await this.getById(recentId),
-			recentScreenshots: await this.getScreenshots(recentId),
-			dailyScreenshots : await this.getScreenshots(dailyId)
-		}
+	if (mappedData.publishers) {
+		mappedData.publishers = mappedData.publishers.map(p => ParserUtils
+		.mapToCamelCase(p) as Publisher)
 	}
 
-	public mapToGame(data: any): Game {
-		const mappedData = ParserUtils.mapToCamelCase(data) as Game
-
-		if (mappedData.platforms !== undefined) {
-			mappedData.platforms
-				= this.mapToGamesPlatforms(mappedData.platforms)
-		}
-
-		if (mappedData.developers) {
-			mappedData.developers = mappedData.developers.map(d => ParserUtils
-			.mapToCamelCase(d) as Developer)
-		}
-
-		if (mappedData.publishers) {
-			mappedData.publishers = mappedData.publishers.map(p => ParserUtils
-			.mapToCamelCase(p) as Publisher)
-		}
-
-		if (mappedData.tags) {
-			mappedData.tags = mappedData.tags.map(t => ParserUtils
-			.mapToCamelCase(t) as Tag)
-		}
-
-		if (mappedData.genres) {
-			mappedData.genres = mappedData.genres.map(g => ParserUtils
-			.mapToCamelCase(g) as Genre)
-		}
-
-		if (mappedData.stores) {
-			mappedData.stores = this.mapToStores(mappedData.stores)
-		}
-
-		return mappedData
+	if (mappedData.tags) {
+		mappedData.tags = mappedData.tags.map(t => ParserUtils
+		.mapToCamelCase(t) as Tag)
 	}
 
-	public mapToStores(stores: Store[]): Store[] {
-		return stores.map(store => {
-			store.store
-				= ParserUtils.mapToCamelCase(store.store) as typeof store.store
-
-			return store
-		})
+	if (mappedData.genres) {
+		mappedData.genres = mappedData.genres.map(g => ParserUtils
+		.mapToCamelCase(g) as Genre)
 	}
 
-	public mapToGamesPlatforms(platforms: GamesPlatform[]): GamesPlatform[] {
-		return platforms.map(mainPlatform => {
-			mainPlatform.platform = ParserUtils
-			.mapToCamelCase(mainPlatform.platform) as Platform
-
-			return ParserUtils.mapToCamelCase(mainPlatform) as GamesPlatform
-		})
+	if (mappedData.stores) {
+		mappedData.stores = mapToStores(mappedData.stores)
 	}
 
-	public async getScreenshots(id: number): Promise<Screenshots> {
-		const routeUrl = ApiService.createRouteUrl(`games/${id}/screenshots`)
-		const response = await ApiService.gameApi.get(routeUrl)
+	return mappedData
+}
 
-		return response.data as Screenshots
-	}
+function mapToStores(stores: Store[]): Store[] {
+	return stores.map(store => {
+		store.store
+			= ParserUtils.mapToCamelCase(store.store) as typeof store.store
+
+		return store
+	})
+}
+
+function mapToGamesPlatforms(platforms: GamesPlatform[]): GamesPlatform[] {
+	return platforms.map(mainPlatform => {
+		mainPlatform.platform = ParserUtils
+		.mapToCamelCase(mainPlatform.platform) as Platform
+
+		return ParserUtils.mapToCamelCase(mainPlatform) as GamesPlatform
+	})
+}
+
+async function getScreenshots(id: number): Promise<Screenshots> {
+	const routeUrl = ApiService.createRouteUrl(`games/${id}/screenshots`)
+	const response = await ApiService.gameApi.get(routeUrl)
+
+	return response.data as Screenshots
 }
 
 
