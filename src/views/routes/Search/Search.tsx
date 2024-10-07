dc7131899ff919b8f02153eadef4f2352b13a67b
trsaints <thiago.dev2k3@gmail.com>
2024-10-07 11:54:18 -0300

    update: consume api middleware instead of data service

--- src/views/routes/Search/Search.tsx
+++ src/views/routes/Search/Search.tsx
@@ -1,24 +1,23 @@
-import { RootContext }           from '@data/context'
-import { GameCard }              from '@views/components'
-import { SearchFilter }          from '@views/components/SearchFilter'
-import { useContext, useEffect } from 'react'
-import style                     from './Search.module.scss'
+import { RootContext }                     from '@data/context'
+import { Genre, Platform, Publisher, Tag } from '@data/types'
+import { db }                              from '@src/main.tsx'
+import { GameCard }                        from '@views/components'
+import { SearchFilter }                    from '@views/components/SearchFilter'
+import { useContext, useEffect }           from 'react'
+import style                               from './Search.module.scss'
 
 
 function Search() {
 	const {
 			  games,
-			  genreService,
 			  genres,
-			  platformService,
 			  platforms,
-			  publisherService,
 			  publishers,
+			  apiMiddleware,
 			  setGenres,
 			  setPlatforms,
 			  setPublishers,
 			  setTags,
-			  tagService,
 			  tags
 		  } = useContext(RootContext)
 
@@ -28,10 +27,14 @@ function Search() {
 									</li>))
 
 	useEffect(() => {
-		publisherService?.getAll({}).then(p => setPublishers(p))
-		platformService?.getAll({}).then(p => setPlatforms(p))
-		tagService?.getAll({}).then(t => setTags(t))
-		genreService?.getAll({}).then(g => setGenres(g))
+		apiMiddleware?.getAll('publishers', db)
+					 .then(apiData => setPublishers(apiData as Publisher[]))
+		apiMiddleware?.getAll('platforms', db)
+					 .then(apiData => setPlatforms(apiData as Platform[]))
+		apiMiddleware?.getAll('tags', db)
+					 .then(apiData => setTags(apiData as Tag[]))
+		apiMiddleware?.getAll('genres', db)
+					 .then(apiData => setGenres(apiData as Genre[]))
 	}, [])
 
 	return (
