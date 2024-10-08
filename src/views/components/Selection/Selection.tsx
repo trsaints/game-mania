a0cee0ca20c4db728c5431f9ed7a74e0ba4d1a4d
trsaints <thiago.dev2k3@gmail.com>
2024-10-08 10:27:58 -0300

    feat: filter Home page games list upon selection of a given Genre

--- src/views/components/Selection/Selection.tsx
+++ src/views/components/Selection/Selection.tsx
@@ -1,22 +1,42 @@
-import { Game, Genre } from '@data/types'
-import { GameCard }    from '@views/components'
-import { ISelection }  from '@views/components/Selection/ISelection.tsx'
-import style           from '@views/routes/Home/Home.module.scss'
-import { Link }        from 'react-router-dom'
+import { Game, Genre }              from '@data/types'
+import { GameCard }                 from '@views/components'
+import {
+	ISelection
+}                                   from '@views/components/Selection/ISelection.tsx'
+import style                        from '@views/routes/Home/Home.module.scss'
+import * as React                   from 'react'
+import { ComponentProps, useState } from 'react'
+import { Link }                     from 'react-router-dom'
 
 
 export function Selection({ games, genres }: ISelection) {
+	const [selectedGenre, setSelectedGenre] = useState<string>('action')
+
+	const filteredGames = games.filter(g =>
+										   g.genres.findIndex(t => t.slug
+																   === selectedGenre)
+										   !== -1)
+	
+	const filterByGenre = (e: React.MouseEvent) => {
+
+		const target   = e.target as HTMLElement
+		const listItem = target.closest('[data-slug]') as HTMLLIElement
+		
+		if (!listItem) return 
+		
+		setSelectedGenre(listItem.dataset['slug'] ?? '')
+	}
 
 	return (
-		<article className={style.Selection}>
+		<article className={style.Selection} onClick={filterByGenre}>
 			<h3>Navigate by genre</h3>
 
 			<GenreFilter genres={genres}/>
-			<GameList games={games}/>
+			<GameList games={filteredGames}/>
 		</article>)
 }
 
-interface IGenreFilter {
+interface IGenreFilter extends ComponentProps<'menu'> {
 	genres: Genre[]
 }
 
@@ -25,7 +45,7 @@ function GenreFilter({ genres }: IGenreFilter) {
 		const key = `selection-${typeof (g)}-${g.id}`
 
 		return (
-			<li key={key}>
+			<li key={key} data-slug={g.slug}>
 				<button className={style.Option}>{g.name}</button>
 			</li>
 		)
