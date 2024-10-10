217b02e280ff79a00bf563668a3a4cc0120180fd
trsaints <thiago.dev2k3@gmail.com>
2024-10-10 09:45:07 -0300

    remove: remove unnecessary overridden signature

--- src/services/interfaces/IGameService.ts
+++ src/services/interfaces/IGameService.ts
@@ -1,10 +1,7 @@
-import { DataRequestParams } from '@data/requests'
 import { Game, Screenshots } from '@data/types'
 import { IDataService }      from '@services/interfaces'
 
 
 export interface IGameService extends IDataService<Game> {
-	getAll(params: DataRequestParams): Promise<Game[]>
-	
 	getScreenshots(id: number): Promise<Screenshots>
 }
