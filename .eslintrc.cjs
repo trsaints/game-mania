10612ead3c43d80bf80049f7449b9ce7dc641a60
trsaints <thiago.dev2k3@gmail.com>
2024-10-18 16:56:22 -0300

    fix: match expected format for plugin usage

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,9 @@
 module.exports = {
   ignores: ["node_modules/**"],
   files: ["**/*.ts", "**/*.tsx"],
-  plugins: ["@typescript-eslint"],
+  plugins: {
+    "@typescript-eslint": require("@typescript-eslint/eslint-plugin")
+  },
   rules: {
     semi: "off",
     "@typescript-eslint/semi": ["error", "never"],
