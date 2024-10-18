91aa60a736d470186adf1703e7e27cfcff7dfbdb
trsaints <thiago.dev2k3@gmail.com>
2024-10-18 16:52:52 -0300

    fix: remove the `override` key which is not supported by the workflow configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,10 @@
 module.exports = {
   ignores: ["node_modules/**"],
-  overrides: [
-    {
-      files: ["**/*.ts", "**/*.tsx"],
-      parser: "@typescript-eslint/parser",
-      plugins: ["@typescript-eslint"],
-      rules: {
-        semi: "off",
-        "@typescript-eslint/semi": ["error", "never"],
-      },
-    },
-  ],
+  files: ["**/*.ts", "**/*.tsx"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    semi: "off",
+    "@typescript-eslint/semi": ["error", "never"],
+  }
 };
