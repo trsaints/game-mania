0c14972f12e8e7ca1ad3feb50f3950c46d944171
trsaints <thiago.dev2k3@gmail.com>
2024-10-24 11:18:46 -0300

    feat: render secondary suggestions within the GamePage component

--- src/views/pages/GamePage/GamePage.tsx
+++ src/views/pages/GamePage/GamePage.tsx
@@ -1,24 +1,28 @@
 import style from './GamePage.module.scss'
 import { useContext, useEffect, useState } from 'react'
 import { RootContext } from '@data/context'
-import { useLoaderData } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
 import { Game } from '@data/types'
-import { GamePanel } from '@views/components'
+import { GamePageList, GamePanel } from '@views/components'
 
 
 function GamePage() {
-	const [selectedGame, setSelectedGame] = useState<Game>()
-	const { apiMiddleware }               = useContext(RootContext)
+	const [selectedGame, setSelectedGame]    = useState<Game>()
+	const { apiMiddleware, games, setGames } = useContext(RootContext)
 
 	type GameId = {
 		id: string
 	}
 
-	const gameId = useLoaderData() as GameId
+	const gameId     = useLoaderData() as GameId
+	const gameGenres = selectedGame?.genres.join(',')
 
 	useEffect(() => {
 		apiMiddleware?.getById('games', Number(gameId.id)).then(game => {
 			setSelectedGame(game as Game)
+		})
+		apiMiddleware?.getAll('games', { genres: gameGenres }).then(games => {
+			setGames(games as Game[])
 		})
 	}, [])
 
@@ -26,15 +30,28 @@ function GamePage() {
 						 ?? selectedGame?.screenshots?.results
 						 ?? []
 
+	const gameGenre = selectedGame?.genres[0].name
+
 	return (
 		<article className={style.GamePage}>
 			{
-				selectedGame ? (
-								 <>
-									 <GamePanel game={selectedGame} images={imagesToLoad}/>
-								 </>
-							 )
-							 : <h2>loading...</h2>
+				selectedGame
+				&& games ? (
+					<>
+						<GamePanel game={selectedGame} images={imagesToLoad}/>
+
+						<aside>
+							<h3>More {gameGenre} games</h3>
+
+							<GamePageList games={games.slice(0, 20)}/>
+							<Link to="/search">
+								See all {gameGenre} games
+							</Link>
+						</aside>
+
+					</>
+				)
+						 : <h2>loading...</h2>
 			}
 		</article>
 	)
