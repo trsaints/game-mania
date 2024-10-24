8dd444a845a257044940fcfbd4c19a7f5621b090
trsaints <thiago.dev2k3@gmail.com>
2024-10-23 23:25:25 -0300

    feat: create a filter for loading missing screenshots due to route discrepancy

---
+++ src/filters/ApiMiddlewareFilter.ts
@@ -0,0 +1,30 @@
+import {
+	IIApiMiddlewareFilter
+} from '@src/filters/interfaces/IIApiMiddlewareFilter.ts'
+import { Game } from '@data/types'
+import { ApiData, ILocalDb } from '@data/local-storage'
+import { IGameService } from '@src/services'
+
+
+export const ApiMiddlewareFilter: IIApiMiddlewareFilter = {
+	mapMissingScreenshots
+}
+
+async function mapMissingScreenshots(game: Game,
+									 gameService: IGameService,
+									 database: ILocalDb<ApiData>
+): Promise<Game> {
+	if (game.shortScreenshots) return Promise.resolve(game)
+
+	const screenshots      = await gameService.getScreenshots(game.id)
+	const successfulUpdate = await database.updateObject('games', game.id, {
+		...game,
+		screenshots
+	})
+
+	if (! successfulUpdate) {
+		return Promise.reject('Failed to update game with missing screenshots')
+	}
+
+	return Promise.resolve(game)
+}
---
+++ src/filters/interfaces/IIApiMiddlewareFilter.ts
@@ -0,0 +1,11 @@
+import { Game } from '@data/types'
+import { ApiData, ILocalDb } from '@data/local-storage'
+import { IGameService } from '@src/services'
+
+
+export interface IIApiMiddlewareFilter {
+	mapMissingScreenshots(game: Game,
+						  gameService: IGameService,
+						  database: ILocalDb<ApiData>
+	): Promise<Game>
+}
