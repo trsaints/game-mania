f5b71886c91e3607e8890bc029392667a2017fbc
trsaints <thiago.dev2k3@gmail.com>
2024-10-31 10:06:32 -0300

    refactor: replace direct useEffect call with new custom hook

--- src/views/routes/Search/Search.tsx
+++ src/views/routes/Search/Search.tsx
@@ -1,40 +1,14 @@
 import { RootContext } from '@data/context'
-import { Game, Genre, Platform, Publisher, Tag } from '@data/types'
-import { useContext, useEffect } from 'react'
+import { useContext } from 'react'
 import style from './Search.module.scss'
 import { Outlet } from 'react-router-dom'
+import { useSearchOptions } from '@src/hooks/useSearchOptions.ts'
 
 
 export { Search }
 
 function Search() {
-	const {
-			  games,
-			  apiMiddleware,
-			  setGenres,
-			  setGames,
-			  setPlatforms,
-			  setPublishers,
-			  setTags
-		  } = useContext(RootContext)
-
-	useEffect(() => {
-		apiMiddleware?.getAll('publishers', {})
-			.then(apiData => setPublishers(apiData as Publisher[]))
-		apiMiddleware?.getAll('platforms', {})
-			.then(apiData => setPlatforms(apiData as Platform[]))
-		apiMiddleware?.getAll('tags', {})
-			.then(apiData => setTags(apiData as Tag[]))
-		apiMiddleware?.getAll('genres', {})
-			.then(apiData => setGenres(apiData as Genre[]))
-
-		if (games) {
-			return
-		}
-
-		apiMiddleware?.getAll('games', { pageSize: 100, metacritic: '80,100' })
-			.then(apiData => setGames(apiData as Game[]))
-	}, [])
+	useSearchOptions(useContext(RootContext))
 
 	return (
 		<main className={style.Search}>
