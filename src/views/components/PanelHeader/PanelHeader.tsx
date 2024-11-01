343c21f10c3917304538f7e28b13e1833828a19e
trsaints <thiago.dev2k3@gmail.com>
2024-11-01 08:40:35 -0300

    refactor: extract implicit GameGenreTags component

--- src/views/components/PanelHeader/PanelHeader.tsx
+++ src/views/components/PanelHeader/PanelHeader.tsx
@@ -1,16 +1,9 @@
 import style from '@views/components/GamePanel/GamePanel.module.scss'
 import { IPanelHeader } from '@views/components/PanelHeader/IPanelHeader.ts'
+import { Game } from '@data/types'
 
 
 export function PanelHeader({ game }: IPanelHeader) {
-	const gameGenres = game.genres
-						   .slice(0, 3)
-						   .map(g =>
-									<li className={style.GenreTag}
-										key={`t-${g.id}`}
-									>{g.name}</li>
-						   )
-
 	return (
 		<header className={style.Header}>
 			<h3>{game.name}</h3>
@@ -26,7 +19,23 @@ export function PanelHeader({ game }: IPanelHeader) {
 
 			<p className={style.Description}>{game.descriptionRaw}</p>
 
-			<ul className={style.Tags}>{gameGenres}</ul>
+			<GameGenreTags game={game}/>
 		</header>
 	)
+}
+
+interface IGameGenreTags {
+	game: Game
+}
+
+function GameGenreTags({ game }: IGameGenreTags) {
+	const gameGenres = game.genres
+						   .slice(0, 3)
+						   .map(g =>
+									<li className={style.GenreTag}
+										key={`t-${g.id}`}
+									>{g.name}</li>
+						   )
+
+	return <ul className={style.Tags}>{gameGenres}</ul>
 }
