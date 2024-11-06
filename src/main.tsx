a7859ab1439c23aa57297c6ad0c8b17e560fdd49
trsaints <thiago.dev2k3@gmail.com>
2024-11-06 09:45:38 -0300

    refactor: move Home component to pages view layer

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { ErrorPage, GamePage, SearchPage } from '@views/pages'
 import { Root, Search } from '@views/routes'
-import { Home } from '@views/routes/Home'
+import { Home } from 'src/views/pages/Home'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
