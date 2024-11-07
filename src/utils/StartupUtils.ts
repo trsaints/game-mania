433c55250c4634e55ca7491c5cb19a917bd5705e
trsaints <thiago.dev2k3@gmail.com>
2024-11-07 07:33:18 -0300

    update: put method declaration within IStartupUtils object instance

--- src/utils/StartupUtils.ts
+++ src/utils/StartupUtils.ts
@@ -11,52 +11,51 @@ import { IStartupUtils } from '@utils/interfaces/IStartupUtils.ts'
 
 
 export const StartupUtils: IStartupUtils = {
-	initializeDb
+	async initializeDb(db: ILocalDb<ApiData>): Promise<boolean> {
+		const gameSchema: LocalDbStore<Game> = {
+			name         : 'games',
+			keyPath      : 'id',
+			autoIncrement: false,
+			indices      : []
+		}
+
+		const genreSchema: LocalDbStore<Genre> = {
+			name         : 'genres',
+			keyPath      : 'id',
+			autoIncrement: false,
+			indices      : []
+		}
+
+		const platformSchema: LocalDbStore<Platform> = {
+			name         : 'platforms',
+			keyPath      : 'id',
+			autoIncrement: false,
+			indices      : []
+		}
+
+		const tagSchema: LocalDbStore<Tag> = {
+			name         : 'tags',
+			keyPath      : 'id',
+			autoIncrement: false,
+			indices      : []
+		}
+
+		const publisherSchema: LocalDbStore<Publisher> = {
+			name         : 'publishers',
+			keyPath      : 'id',
+			autoIncrement: false,
+			indices      : []
+		}
+
+		const isCreated = await db.create([
+											  gameSchema,
+											  platformSchema,
+											  genreSchema,
+											  tagSchema,
+											  publisherSchema
+										  ])
+
+		return Promise.resolve(isCreated)
+	}
 }
 
-async function initializeDb(db: ILocalDb<ApiData>): Promise<boolean> {
-	const gameSchema: LocalDbStore<Game> = {
-		name         : 'games',
-		keyPath      : 'id',
-		autoIncrement: false,
-		indices      : []
-	}
-
-	const genreSchema: LocalDbStore<Genre> = {
-		name         : 'genres',
-		keyPath      : 'id',
-		autoIncrement: false,
-		indices      : []
-	}
-
-	const platformSchema: LocalDbStore<Platform> = {
-		name         : 'platforms',
-		keyPath      : 'id',
-		autoIncrement: false,
-		indices      : []
-	}
-
-	const tagSchema: LocalDbStore<Tag> = {
-		name         : 'tags',
-		keyPath      : 'id',
-		autoIncrement: false,
-		indices      : []
-	}
-
-	const publisherSchema: LocalDbStore<Publisher> = {
-		name         : 'publishers',
-		keyPath      : 'id',
-		autoIncrement: false,
-		indices      : []
-	}
-
-	const isCreated = await db.create([
-										  gameSchema,
-										  platformSchema,
-										  genreSchema,
-										  tagSchema,
-										  publisherSchema
-									  ])
-
-	return Promise.resolve(isCreated)
-}
