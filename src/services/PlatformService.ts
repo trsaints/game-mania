fd1f52b71e7fb83db49a4cddb2c53d0cf8e61be0
trsaints <thiago.dev2k3@gmail.com>
2024-11-16 22:37:17 -0300

    refactor: replace local constant with class implementation

--- src/services/PlatformService.ts
+++ src/services/PlatformService.ts
@@ -1,27 +1,36 @@
 import { DataRequestParams } from '@data/request-parameters'
 import { Platform } from '@data/types'
 import { IApiService, IDataService } from '@src/services'
-import { ParserUtils } from '@src/utils'
+import { IParserUtils } from '@src/utils'
 
 
-export const PlatformService: IDataService<Platform> = {
-	getAll,
-	getById
+export class PlatformService implements IDataService<Platform> {
+	constructor(parserUtils: IParserUtils) {
+		this._parserUtils = parserUtils
+	}
+
+	private readonly _parserUtils: IParserUtils
+
+	async getAll(params: DataRequestParams,
+				 apiService: IApiService
+	): Promise<Platform[]> {
+		const baseUrl  = apiService.createRouteUrl('platforms')
+		const response = await apiService.gameApi.get(baseUrl, {
+			params: params
+					? this._parserUtils.mapToSnakeCase(params as never)
+					: {}
+		})
+
+		return response?.data?.results.map(
+				   (r: never) => this._parserUtils.mapToCamelCase(r)) as Platform[]
+			   ?? []
+	}
+
+	async getById(id: number, apiService: IApiService): Promise<Platform> {
+		const baseUrl  = apiService.createRouteUrl(`platforms/${id}`)
+		const response = await apiService.gameApi.get(baseUrl)
+
+		return this._parserUtils.mapToCamelCase(response?.data as never) as Platform
+	}
 }
 
-async function getAll(params: DataRequestParams, apiService: IApiService): Promise<Platform[]> {
-	const baseUrl  = apiService.createRouteUrl('platforms')
-	const response = await apiService.gameApi.get(baseUrl, {
-		params: params ? ParserUtils.mapToSnakeCase(params as never) : {}
-	})
-
-	return response?.data?.results.map(
-		(r: never) => ParserUtils.mapToCamelCase(r)) as Platform[] ?? []
-}
-
-async function getById(id: number, apiService: IApiService): Promise<Platform> {
-	const baseUrl  = apiService.createRouteUrl(`platforms/${id}`)
-	const response = await apiService.gameApi.get(baseUrl)
-
-	return ParserUtils.mapToCamelCase(response?.data as never) as Platform
-}
