2a9369f39b1988fd4a0ce38f881698e73d00e033
trsaints <thiago.dev2k3@gmail.com>
2024-11-16 23:00:09 -0300

    refactor: replace local constant with class implementation

--- src/services/GameService.ts
+++ src/services/GameService.ts
@@ -2,26 +2,36 @@ import { DataRequestParams } from 'src/data/request-parameters'
 import { Game, Screenshots } from '@data/types'
 import { IApiService } from '@src/services'
 import { IGameService } from '@services/interfaces'
-import { ParserUtils, TypeUtils } from '@src/utils'
+import { IParserUtils, ITypeUtils } from '@src/utils'
 
 
 export class GameService implements IGameService {
+	constructor(parserUtils: IParserUtils, typeUtils: ITypeUtils) {
+		this._parserUtils = parserUtils
+		this._typeUtils   = typeUtils
+	}
+
+	private readonly _parserUtils: IParserUtils
+	private readonly _typeUtils: ITypeUtils
+
 	async getAll(params: DataRequestParams,
 				 apiService: IApiService
 	): Promise<Game[]> {
 		const baseUrl  = apiService.createRouteUrl('games')
 		const response = await apiService.gameApi.get(baseUrl, {
-			params: params ? ParserUtils.mapToSnakeCase(params as never) : {}
+			params: params
+					? this._parserUtils.mapToSnakeCase(params as never)
+					: {}
 		})
 
-		return response.data?.results.map(TypeUtils.mapToGame) ?? []
+		return response.data?.results.map(this._typeUtils.mapToGame) ?? []
 	}
 
 	async getById(id: number, apiService: IApiService): Promise<Game> {
 		const gameUrl  = apiService.createRouteUrl(`games/${id}`)
 		const response = await apiService.gameApi.get(gameUrl)
 
-		return TypeUtils.mapToGame(response.data as never)
+		return this._typeUtils.mapToGame(response.data as never)
 	}
 
 	async getScreenshots(id: number,
