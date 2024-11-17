a341e2b81fdf66422583768491bc13947a021b32
trsaints <thiago.dev2k3@gmail.com>
2024-11-16 23:05:52 -0300

    refactor: replace local constant with class implementation

--- src/view-models/RootViewModel.ts
+++ src/view-models/RootViewModel.ts
@@ -9,31 +9,30 @@ import {
 import { LocalDb } from '@data/local-storage'
 import { ApiMiddleware } from '@src/middlewares'
 import { ApiMiddlewareFilter } from '@src/filters'
-import { StartupUtils } from '@src/utils'
+import { ParserUtils, StartupUtils, TypeUtils } from '@src/utils'
 import { IRootViewModel } from '@src/view-models/interfaces/IRootViewModel.ts'
 import { ApiService } from '@services/ApiService.ts'
 
 
 class RootViewModel implements IRootViewModel {
+	private readonly _parserUtils = new ParserUtils()
+	private readonly _typeUtils   = new TypeUtils(this._parserUtils)
+
 	dataServiceDictionary: DataServiceDictionary = {
-		games     : new GameService(),
-		genres    : GenreService,
-		platforms : PlatformService,
-		publishers: PublisherService,
-		tags      : TagService
+		games     : new GameService(this._parserUtils, this._typeUtils),
+		genres    : new GenreService(this._parserUtils),
+		platforms : new PlatformService(this._parserUtils),
+		publishers: new PublisherService(this._parserUtils),
+		tags      : new TagService(this._parserUtils)
 	}
 
-	apiService    = new ApiService()
-	localDb       = new LocalDb('game-mania', 1)
-	apiMiddleware = new ApiMiddleware(this.dataServiceDictionary,
-									  this.apiService,
-									  this.localDb,
-									  ApiMiddlewareFilter
+	public readonly apiService    = new ApiService()
+	public readonly localDb       = new LocalDb('game-mania', 1)
+	public readonly apiMiddleware = new ApiMiddleware(this.dataServiceDictionary,
+													  this.apiService,
+													  this.localDb,
+													  ApiMiddlewareFilter
 	)
-
-	constructor() {
-		// Initialization moved to initializeDb method
-	}
 
 	async initializeDb(): Promise<void> {
 		if (this.localDb.isCreated()) return Promise.resolve()
