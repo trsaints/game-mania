920615dcc78d344747c7c0ac5542b95830553f82
trsaints <thiago.dev2k3@gmail.com>
2024-11-16 23:19:45 -0300

    refactor: replace local constant with class implementation

--- src/utils/StylingUtils.ts
+++ src/utils/StylingUtils.ts
@@ -2,18 +2,17 @@ import { Game, InlineBannerStyle } from '@data/types'
 import { IStylingUtils } from '@utils/interfaces'
 
 
-export const StylingUtils: IStylingUtils = {
-	getInlineBanner
-}
-
-function getInlineBanner(game?: Game): InlineBannerStyle {
-	return (
-		{
-			background: `linear-gradient(to bottom, 
+export class StylingUtils implements IStylingUtils {
+	getInlineBanner(game?: Game): InlineBannerStyle {
+		return (
+			{
+				background: `linear-gradient(to bottom, 
 										 rgba(0, 0, 0, 0.8), 
 										 rgba(0, 0, 0, 0.8)) 100%,
 						 				 url(${game?.backgroundImage}) 
 						 				 no-repeat center / cover`
-		}
-	)
+			}
+		)
+	}
 }
+
