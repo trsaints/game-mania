bc55a5f396fd6c2b36bc1ce955b8df3faac68078
trsaints <thiago.dev2k3@gmail.com>
2024-11-24 12:19:01 -0300

    fix: update invalid content structure for matching spec

--- src/views/components/PanelHeader/PanelHeader.tsx
+++ src/views/components/PanelHeader/PanelHeader.tsx
@@ -1,36 +1,37 @@
 import style from '@views/components/GamePanel/GamePanel.module.scss'
 import { IPanelHeader } from '@views/components/PanelHeader/IPanelHeader.ts'
-import {
-	GameTags
-} from '@views/components/GameGenreTags/GameTags.tsx'
+import { GameTags } from '@views/components/GameGenreTags/GameTags.tsx'
 
 
 export function PanelHeader({ game }: IPanelHeader) {
 	const shortDescriptionPattern = /^(([^.]+\.){1,4})/
-	const shortDescription = game.descriptionRaw.match(shortDescriptionPattern)?.[0]
+	const shortDescription        = game.descriptionRaw.match(
+		shortDescriptionPattern)?.[0]
 
 	return (
 		<header className={style.Header}>
 			<h3>{game.name}</h3>
 
 			<dl className={style.MetaData}>
+				<dt>release date:</dt>
 				<dd className={style.Meta}>
-					<dt>release date:</dt>
 
 					<time dateTime={game.released}>
 						{new Date(game.released).getFullYear()}
 					</time>
 				</dd>
+			</dl>
 
-
+			<dl className={style.MetaData}>
+				<dt>game publisher:</dt>
 				<dd className={style.Meta}>
-					<dt>game publisher:</dt>
 					{game?.publishers[0]?.name ?? 'NA'}
 				</dd>
+			</dl>
 
-
+			<dl className={style.MetaData}>
+				<dt>game studio:</dt>
 				<dd className={style.Meta}>
-					<dt>game studio:</dt>
 					{game?.developers[0]?.name ?? 'NA'}
 				</dd>
 			</dl>
