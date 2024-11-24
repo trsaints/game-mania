a6e49343b3259dee4b16c1ae919c7d336d49a69e
trsaints <thiago.dev2k3@gmail.com>
2024-11-24 10:11:30 -0300

    feat: add a button for switching the visibility of SearchFilter

--- src/views/pages/SearchPage/SearchPage.tsx
+++ src/views/pages/SearchPage/SearchPage.tsx
@@ -1,12 +1,14 @@
 import style from './SearchPage.module.scss'
 import { GamePageList, SearchFilter } from '@views/components'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { RootContext } from '@data/context'
 
 
 export { SearchPage }
 
 function SearchPage() {
+	const [isVisible, setIsVisible] = useState<boolean>(false)
+
 	const {
 			  games,
 			  genres,
@@ -15,15 +17,32 @@ function SearchPage() {
 			  tags
 		  } = useContext(RootContext)
 
+	const filterSelector = isVisible
+						   ? `${style.SearchControls} ${style.filterVisible}`
+						   : style.SearchControls
+
 	return (
 		<div className={style.SearchPage}>
 			{(genres && publishers && platforms && tags)
-			 && <SearchFilter
-				 publishers={publishers}
-				 platforms={platforms}
-				 genres={genres}
-				 tags={tags}
-			 />}
+			 && (
+				 <aside className={filterSelector}>
+					 <SearchFilter
+						 publishers={publishers}
+						 platforms={platforms}
+						 genres={genres}
+						 tags={tags}
+					 />
+
+					 <menu>
+						 <li>
+							 <button className={style.Switch}
+									 onClick={() => setIsVisible(! isVisible)}
+									 type="button"></button>
+						 </li>
+					 </menu>
+				 </aside>
+			 )
+			}
 
 			{games && <GamePageList games={games}/>}
 		</div>
