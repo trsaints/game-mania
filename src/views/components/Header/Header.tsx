7ca89279508fc0166182ebd24c45e56a625ac521
trsaints <thiago.dev2k3@gmail.com>
2024-12-01 12:48:28 -0300

    refactor: extract sub component

--- src/views/components/Header/Header.tsx
+++ src/views/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import style from './Header.module.scss'
-import { useState } from 'react'
+import { Dispatch, SetStateAction, useState } from 'react'
 import { HeaderViewModel } from '@src/view-models/HeaderViewModel.ts'
 
 
@@ -28,39 +28,49 @@ function Header() {
 				</li>
 
 				<li>
-					<dialog className={style.MainMenu}
-							data-dialog="main-menu"
-							onKeyDown={(e) => {
-								viewModel.closeOnEscape(e, setIsMenuOpen)
-							}}>
-						<h2 className="sr-only">main menu</h2>
-
-						<menu>
-							<li>
-								<button className={style.CloseButton}
-										type="button"
-										onClick={() => {
-											viewModel.closeOnClick(
-												setIsMenuOpen)
-										}}>
-									<span className="sr-only">close menu</span>
-									x
-								</button>
-							</li>
-						</menu>
-
-						<nav className={style.RouteOptions}
-							 onClick={(e) => {
-								 viewModel.closeOnRouteChange(e, setIsMenuOpen)
-							 }}
-						>
-							<Link data-link="route" to="/">home</Link>
-							<Link data-link="route" to="/search">search</Link>
-						</nav>
-					</dialog>
+					<MainMenu setIsMenuOpen={setIsMenuOpen}/>
 				</li>
 			</menu>
 		</header>
+	)
+}
+
+interface IMainMenu {
+	setIsMenuOpen: Dispatch<SetStateAction<boolean>>
+}
+
+function MainMenu({ setIsMenuOpen }: IMainMenu) {
+	return (
+		<dialog className={style.MainMenu}
+				data-dialog="main-menu"
+				onKeyDown={(e) => {
+					viewModel.closeOnEscape(e, setIsMenuOpen)
+				}}>
+			<h2 className="sr-only">main menu</h2>
+
+			<menu>
+				<li>
+					<button className={style.CloseButton}
+							type="button"
+							onClick={() => {
+								viewModel.closeOnClick(
+									setIsMenuOpen)
+							}}>
+						<span className="sr-only">close menu</span>
+						x
+					</button>
+				</li>
+			</menu>
+
+			<nav className={style.RouteOptions}
+				 onClick={(e) => {
+					 viewModel.closeOnRouteChange(e, setIsMenuOpen)
+				 }}
+			>
+				<Link data-link="route" to="/">home</Link>
+				<Link data-link="route" to="/search">search</Link>
+			</nav>
+		</dialog>
 	)
 }
 
