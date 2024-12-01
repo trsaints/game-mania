7a1efa72ff5e17ee74a9f717779e0076112cf653
trsaints <thiago.dev2k3@gmail.com>
2024-12-01 12:57:21 -0300

    update: replace dialog menu with dropdown

--- src/views/components/Header/Header.tsx
+++ src/views/components/Header/Header.tsx
@@ -1,14 +1,8 @@
 import style from './Header.module.scss'
-import { useState } from 'react'
-import { HeaderViewModel } from '@src/view-models/HeaderViewModel.ts'
 import { MainMenu } from '@views/components/Header/MainMenu.tsx'
 
 
-const viewModel = new HeaderViewModel()
-
 function Header() {
-	const [isMenuOpen, setIsMenuOpen] = useState(false)
-
 	return (
 		<header className={style.Header}>
 			<h1 className="sr-only">Game Mania</h1>
@@ -19,17 +13,7 @@ function Header() {
 
 			<menu className={style.Links}>
 				<li>
-					<button className={style.MenuButton}
-							onClick={() => viewModel.toggleMenu(isMenuOpen,
-																setIsMenuOpen
-							)}
-							type="button">{viewModel.toggleButtonText(isMenuOpen)}
-					</button>
-				</li>
-
-				<li>
-					<MainMenu setIsMenuOpen={setIsMenuOpen}
-							  parentViewModel={viewModel}/>
+					<MainMenu/>
 				</li>
 			</menu>
 		</header>
--- src/views/components/Header/MainMenu.tsx
+++ src/views/components/Header/MainMenu.tsx
@@ -1,47 +1,21 @@
-import { Dispatch, SetStateAction } from 'react'
 import style from '@views/components/Header/Header.module.scss'
 import { Link } from 'react-router-dom'
-import {
-	IHeaderViewModel
-} from '@src/view-models/interfaces/IHeaderViewModel.ts'
+import { useState } from 'react'
 
 
-export interface IMainMenu {
-	setIsMenuOpen: Dispatch<SetStateAction<boolean>>
-	parentViewModel: IHeaderViewModel
-}
+export function MainMenu() {
+	const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-export function MainMenu({ setIsMenuOpen, parentViewModel }: IMainMenu) {
 	return (
-		<dialog className={style.MainMenu}
-				data-dialog="main-menu"
-				onKeyDown={(e) => {
-					parentViewModel.closeOnEscape(e, setIsMenuOpen)
-				}}>
-			<h2 className="sr-only">main menu</h2>
+		<details className={style.MainMenu}
+				 onClick={() => setIsMenuOpen(! isMenuOpen)}
+		>
+			<summary>{isMenuOpen ? 'open' : 'close'} menu</summary>
 
-			<menu>
-				<li>
-					<button className={style.CloseButton}
-							type="button"
-							onClick={() => {
-								parentViewModel.closeOnClick(
-									setIsMenuOpen)
-							}}>
-						<span className="sr-only">close menu</span>
-						x
-					</button>
-				</li>
-			</menu>
-
-			<nav className={style.RouteOptions}
-				 onClick={(e) => {
-					 parentViewModel.closeOnRouteChange(e, setIsMenuOpen)
-				 }}
-			>
+			<nav className={style.RouteOptions}>
 				<Link data-link="route" to="/">home</Link>
 				<Link data-link="route" to="/search">search</Link>
 			</nav>
-		</dialog>
+		</details>
 	)
 }
