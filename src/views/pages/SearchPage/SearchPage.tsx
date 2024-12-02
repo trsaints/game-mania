259ff8bec77356dccfe62f9ead0189a6241b5586
trsaints <thiago.dev2k3@gmail.com>
2024-12-02 08:47:01 -0300

    refactor: extract subcomponent

--- src/views/pages/SearchPage/SearchPage.tsx
+++ src/views/pages/SearchPage/SearchPage.tsx
@@ -1,21 +1,32 @@
 import style from './SearchPage.module.scss'
 import { GamePageList, SearchFilter } from '@views/components'
 import { useContext, useState } from 'react'
-import { RootContext } from '@data/context'
+import { IRootContext, RootContext } from '@data/context'
 
 
 export { SearchPage }
 
 function SearchPage() {
-	const [isVisible, setIsVisible] = useState<boolean>(false)
+	const rootContext = useContext(RootContext)
 
-	const {
-			  games,
-			  genres,
-			  platforms,
-			  publishers,
-			  tags
-		  } = useContext(RootContext)
+	const { games } = rootContext
+
+
+	return (
+		<div className={style.SearchPage}>
+			<SearchControls context={rootContext}/>
+
+			{games && <GamePageList games={games}/>}
+		</div>
+	)
+}
+
+interface ISearchControls {
+	context: IRootContext
+}
+
+function SearchControls({ context }: ISearchControls) {
+	const [isVisible, setIsVisible] = useState<boolean>(false)
 
 	const filterSelector = isVisible
 						   ? `${style.SearchControls} ${style.filterVisible}`
@@ -23,34 +34,30 @@ function SearchPage() {
 
 	const textSwitch = isVisible ? 'hide' : 'show'
 
-	return (
-		<div className={style.SearchPage}>
-			{(genres && publishers && platforms && tags)
-			 && (
-				 <aside className={filterSelector}>
-					 <SearchFilter
-						 publishers={publishers}
-						 platforms={platforms}
-						 genres={genres}
-						 tags={tags}
-					 />
+	const { genres, publishers, platforms, tags } = context
 
-					 <menu>
-						 <li>
-							 <button className={style.Switch}
-									 onClick={() => setIsVisible(! isVisible)}
-									 type="button">
+	if (! genres || ! publishers || ! platforms || ! tags) return (<></>)
+
+	return (
+		<aside className={filterSelector}>
+			<SearchFilter
+				publishers={publishers}
+				platforms={platforms}
+				genres={genres}
+				tags={tags}
+			/>
+
+			<menu>
+				<li>
+					<button className={style.Switch}
+							onClick={() => setIsVisible(! isVisible)}
+							type="button">
 								 <span className="sr-only">
 									 {textSwitch} filters
 								 </span>
-							 </button>
-						 </li>
-					 </menu>
-				 </aside>
-			 )
-			}
-
-			{games && <GamePageList games={games}/>}
-		</div>
+					</button>
+				</li>
+			</menu>
+		</aside>
 	)
 }
