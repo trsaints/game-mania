5e2201b121bd9713746065e3a586a6e7128f45e6
trsaints <thiago.dev2k3@gmail.com>
2024-12-02 10:05:53 -0300

    fix: remove default hook call for avoiding infinite re-renders

--- src/views/routes/Search/Search.tsx
+++ src/views/routes/Search/Search.tsx
@@ -1,15 +1,10 @@
-import { RootContext } from '@data/context'
-import { useContext } from 'react'
 import style from './Search.module.scss'
 import { Outlet } from 'react-router-dom'
-import { useSearchPage } from '@src/hooks/useSearchPage.ts'
 
 
 export { Search }
 
 function Search() {
-	useSearchPage(useContext(RootContext))
-
 	return (
 		<main className={style.Search}>
 			<h2 id="search-header">Search your next favorite game</h2>
