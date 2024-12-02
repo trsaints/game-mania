4e5ce2fc02e9dd10b6ed1739afcb371a8928a5ca
trsaints <thiago.dev2k3@gmail.com>
2024-12-01 23:15:20 -0300

    remove: temporarily remove view-model for further replacement

--- src/view-models/interfaces/IFilterSortViewModel.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeEvent, Dispatch, SetStateAction } from 'react'
-import { Game } from '@data/types'
-
-
-export interface IFilterSortViewModel {
-	sortResults(e: ChangeEvent,
-				setGames: Dispatch<SetStateAction<Game[] | undefined>>,
-				order: 'asc' | 'desc'): void
-}
