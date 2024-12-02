86814b50bd70795b4e17c35819886035795c2396
trsaints <thiago.dev2k3@gmail.com>
2024-12-02 08:06:55 -0300

    update: restructure layout of subcomponent

--- src/views/components/FilterOrderSwitch/FilterOrderSwitch.tsx
+++ src/views/components/FilterOrderSwitch/FilterOrderSwitch.tsx
@@ -1,20 +1,27 @@
+import style from './FilterOrderSwitch.module.scss'
+
+
 export function FilterOrderSwitch() {
 	return (
-		<fieldset>
-			<legend>order</legend>
+		<fieldset className={style.FilterOrderSwitch}>
+			<legend>order:</legend>
 
-			<label htmlFor="sort-asc">ascending</label>
-			<input type="radio"
-				   name="order"
-				   value="asc"
-				   id="sort-asc"
-				   defaultChecked/>
+			<p>
+				<label htmlFor="sort-asc">ascending</label>
+				<input type="radio"
+					   name="order"
+					   value="asc"
+					   id="sort-asc"
+					   defaultChecked/>
+			</p>
 
-			<label htmlFor="sort-desc">descending</label>
-			<input type="radio"
-				   name="order"
-				   value="desc"
-				   id="sort-desc"/>
+			<p>
+				<label htmlFor="sort-desc">descending</label>
+				<input type="radio"
+					   name="order"
+					   value="desc"
+					   id="sort-desc"/>
+			</p>
 		</fieldset>
 	)
 }
