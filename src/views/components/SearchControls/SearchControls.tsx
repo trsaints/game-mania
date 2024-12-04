24cbe3b882a832bfb930d7b864af8f3cd91c3f6d
trsaints <thiago.dev2k3@gmail.com>
2024-12-03 23:24:51 -0300

    feat: remove local state management for removing out-of-context hook usage

--- src/views/components/SearchControls/SearchControls.tsx
+++ src/views/components/SearchControls/SearchControls.tsx
@@ -4,33 +4,19 @@ import { SearchFilter } from '@views/components'
 import {
 	ControlsClose
 } from '@views/components/ControlsClose/ControlsClose.tsx'
-import { Genre, Platform, Publisher, Tag } from '@data/types'
+import { ISearchFilter } from '@views/components/SearchFilter/ISearchFilter.ts'
 
 
-export interface ISearchControls {
-	genres: Genre[]
-	publishers: Publisher[]
-	platforms: Platform[]
-	tags: Tag[]
-}
-
-export function SearchControls(props: ISearchControls) {
+export function SearchControls(props: ISearchFilter) {
 	const [isVisible, setIsVisible] = useState<boolean>(false)
 
 	const filterSelector = isVisible
 						   ? `${style.SearchControls} ${style.filterVisible}`
 						   : style.SearchControls
 
-	const { genres, publishers, platforms, tags } = props
-
 	return (
 		<aside className={filterSelector}>
-			<SearchFilter
-				publishers={publishers}
-				platforms={platforms}
-				genres={genres}
-				tags={tags}
-			/>
+			<SearchFilter {...props} />
 
 			<ControlsClose isVisible={isVisible} setIsVisible={setIsVisible}/>
 		</aside>
--- src/views/components/SearchFilter/SearchFilter.tsx
+++ src/views/components/SearchFilter/SearchFilter.tsx
@@ -6,32 +6,25 @@ import {
 import {
 	SearchFilterViewModel
 } from '@src/view-models/SearchFilterViewModel.ts'
-import { useContext, useState } from 'react'
-import { DataRequestParams } from '@data/request-parameters'
-import { useSearchPage } from '@src/hooks/useSearchPage.ts'
-import { RootContext } from '@data/context'
 
 
 export { SearchFilter }
 
 function SearchFilter(props: ISearchFilter) {
-	const [filters, setFilters] = useState<DataRequestParams>({})
-	const [order, setOrder]     = useState<'asc' | 'desc'>('asc')
-
-	useSearchPage(useContext(RootContext), order, filters)
-
 	const viewModel = new SearchFilterViewModel(props)
+
+	const { filterStates } = props
 
 	return (
 		<menu className={style.SearchFilter}
-			  onClick={(event) => viewModel.updateFilters(event, setFilters)}>
+			  onClick={(event) => viewModel.updateFilters(event, filterStates.setFilters)}>
 			<li>
 				<FilterSort parentViewModel={viewModel}
-							setFilters={setFilters}/>
+							setFilters={filterStates.setFilters}/>
 			</li>
 
 			<li>
-				<FilterOrderSwitch setOrder={setOrder}/>
+				<FilterOrderSwitch setOrder={filterStates.setOrder}/>
 			</li>
 
 			<MetadataFilters {...viewModel}/>
